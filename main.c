4fe7affaaf36265c21bbedb538f2a13567b24f99
LeonardoAlmd <leonardoalmd8@outlook.com>
2024-05-28 00:56:13 -0300

    modified the move robot and matrix to integer

--- main.c
+++ main.c
@@ -1,30 +1,23 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h> // Para o uso da função sleep
 #include <stdbool.h>
+
 #define SIZE 8
 
 // Declara uma matriz 8x8 de ponteiros para strings
-char *matrix[SIZE][SIZE];
+int *matrix[SIZE][SIZE];
 // Declara variáveis para armazenar a posição inicial do robô
 int stationLine, stationColumn;
 
-// Estrutura para armazenar a posição do lixo
+int trashCounter;
 typedef struct {
     int line;
     int column;
 } Position;
 
-// Estrutura para armazenar a posição atual do robô
-typedef struct {
-    int line;
-    int column;
-} Robot;
-
-Position trash[64]; // Supondo que possa haver até 64 posições de lixo em uma matriz 8x8
-
-Robot robo; // Variável para a posição atual do robô
+Position trash[SIZE * SIZE];
+Position robot;
 
 // Função para inicializar a matriz com valores padrão "***"
 void createMatrix() {
@@ -33,19 +26,44 @@ void createMatrix() {
         // Percorre todas as colunas
         for (int j = 0; j < SIZE; j++) {
             // Define o valor padrão "***" em cada posição
-            matrix[i][j] = "***";
+            matrix[i][j] = 1;
         }
     }
 }
+// 1 = PAREDE
+// 2 = LUGAR QUE PODE PASSAR = LIXO/ESTAÇÃO
+// 0 = LUGAR LIMPO
+// -1 = ROBO
 
 // Função para imprimir a matriz no console
 void printMatrix() {
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+    
     // Percorre todas as linhas
     for (int i = 0; i < SIZE; i++) {
         // Percorre todas as colunas
         for (int j = 0; j < SIZE; j++) {
-            // Imprime o valor da posição atual com formatação
-            printf("|%s| ", matrix[i][j]);
+            if (matrix[i][j] == -1) {
+                printf("|-_-|");
+                //printf("| %d |", matrix[i][j]);
+            } else if (matrix[i][j] == 0) {
+                printf("|   |");
+                //printf("| %d |", matrix[i][j]);
+            } else if (matrix[i][j] == 1) {
+                printf("|---|");
+                //printf("| %d |", matrix[i][j]);
+            } else if (i == stationLine && j == stationColumn) {
+                printf("| E |");
+                //printf("| %d |", matrix[i][j]);
+            } 
+            else if (matrix[i][j] == 2) {
+                printf("|@@@|");
+                //printf("| %d |", matrix[i][j]);
+            }
         }
         // Pula para a próxima linha após imprimir todas as colunas
         printf("\n");
@@ -63,7 +81,7 @@ void addStation() {
     scanf("%d", &stationLine);
     
     // Verifica se a linha está fora do intervalo permitido e solicita novamente se necessário
-    while (stationLine < 0 || stationLine > 7) {
+    while (stationLine < 0 || stationLine > SIZE - 1) {
         printf("Digite a linha inicial entre 0 e 7 do robo: ");
         scanf("%d", &stationLine);
     }
@@ -73,142 +91,95 @@ void addStation() {
     scanf("%d", &stationColumn);
     
     // Verifica se a coluna está fora do intervalo permitido e solicita novamente se necessário
-    while (stationColumn < 0 || stationColumn > 7) {
+    while (stationColumn < 0 || stationColumn > SIZE - 1) {
         printf("Digite a coluna inicial entre 0 e 7 do robo: ");
         scanf("%d", &stationColumn);
     }
     
-    // Define a posição inicial do robô na matriz com " E "
-    matrix[stationLine][stationColumn] = " E ";
+    matrix[stationLine][stationColumn] = 2;
 }
 
-// Função para verificar a direção do robô em relação à base
-int checkDirectionBase() {
-    // Verifica se a base está em uma das direções adjacentes ao robô
-    bool verifyBaseR = robo.column < SIZE - 1 && (strcmp(matrix[robo.line][robo.column + 1], " E ") == 0);
-    bool verifyBaseL = robo.column > 0 && (strcmp(matrix[robo.line][robo.column - 1], " E ") == 0);
-    bool verifyBaseD = robo.line < SIZE - 1 && (strcmp(matrix[robo.line + 1][robo.column], " E ") == 0);
-    bool verifyBaseT = robo.line > 0 && (strcmp(matrix[robo.line - 1][robo.column], " E ") == 0);
-    
-    // Verifica se há um espaço vazio em direção à base
-    bool verifyEmptyR = (robo.column < stationColumn) && (strcmp(matrix[robo.line][robo.column + 1], "   ")) == 0;
-    bool verifyEmptyL = (robo.column > stationColumn) && (strcmp(matrix[robo.line][robo.column - 1], "   ")) == 0;
-    bool verifyEmptyD = (robo.line < stationLine) && (strcmp(matrix[robo.line + 1][robo.column], "   ")) == 0;
-    bool verifyEmptyT = (robo.line > stationLine) && (strcmp(matrix[robo.line - 1][robo.column], "   ")) == 0;
- 
-    // Retorna a direção baseada nas verificações
-    if (verifyBaseR || verifyEmptyR) {
-        return 1;
-    } else if (verifyBaseT || verifyEmptyT) {
-        return 2;
-    } else if (verifyBaseL || verifyEmptyL) {
-        return 3;
-    } else if (verifyBaseD || verifyEmptyD) {
-        return 4;
-    }
-    return 0;
-}
+void verifyTrash() {
+    int i, j;
+    trashCounter = 0; // Resetar contador Lixo antes de verificar novamente
 
-// Função para mover o robô de volta à base
-int returnBase() {
-    // Continua movendo o robô até a base
-    while (1) {
-        int direction = checkDirectionBase();
-        
-        if (!(robo.line == stationLine && robo.column == stationColumn)) {
-            matrix[robo.line][robo.column] = "   ";
+    // Percorre todas as linhas
+    for (i = 0; i < 8; i++) {
+        // Percorre todas as colunas
+        for (j = 0; j < 8; j++) {
+            // Verifica se a posição atual contém lixo
+            if (matrix[i][j] == 2 && !(i == stationLine && j == stationColumn)) {
+                // Armazena a posição do lixo na estrutura lixo
+                trash[trashCounter].line = i;
+                trash[trashCounter].column = j;
+                // Incrementa o contador de lixo
+                trashCounter++;
+            }
         }
-        // Move o robô baseado na direção retornada
-        switch (direction) {
-            case 1: // Direita
-                robo.column += 1;
-                break;
-            case 2: // Esquerda
-                robo.line -= 1;
-                break;
-            case 3: // Baixo
-                robo.column -= 1;
-                break;
-            case 4: // Cima
-                robo.line += 1;
-                break;
-        }
-        // Atualiza a posição do robô na matriz
-        matrix[robo.line][robo.column] = "-_-";
-        system("cls");
-        printMatrix();
-        return 0;
     }
 }
 
-// Função para verificar a direção do lixo em relação ao robô
+
+int isValidMove(int x, int y) {
+    return y < SIZE - 1 && y > 0 && x < SIZE && x > 0;
+}
+
 int checkDirectionTrash() {
-    // Verifica se há lixo em uma das direções adjacentes ao robô
-    int verifyR = robo.column < SIZE - 1 && strcmp(matrix[robo.line][robo.column + 1], "@@@") == 0;
-    int verifyL = robo.column > 0 && strcmp(matrix[robo.line][robo.column - 1], "@@@") == 0;
-    int verifyD = robo.line < SIZE - 1 && strcmp(matrix[robo.line + 1][robo.column], "@@@") == 0;
-    int verifyT = robo.line > 0 && strcmp(matrix[robo.line - 1][robo.column], "@@@") == 0;
-    
-    // Retorna a direção baseada nas verificações
-    if (verifyR) {
-        return 1;
-    } else if (verifyD) {
-        return 2;
-    } else if (verifyL) {
-        return 3;
-    } else if (verifyT) {
-        return 4;
-    } 
-    return 0;
+    // Direções: Cima, Esquerda, Direita, Baixo 
+    int directions[4][2] = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+    for (int i = 0; i < 4; i++) {
+        int newLine = robot.line + directions[i][0];
+        int newColumn = robot.column + directions[i][1];
+        
+        if (isValidMove(newLine, newColumn) && matrix[newLine][newColumn] == 2) {
+            return i + 1; // Retorna a direção correspondente
+        }
+    }
+    return 0; // Nenhum lixo encontrado
 }
 
-// Função para mover o robô e limpar o lixo
 int moveRobot() {
-    // Inicializa a posição do robô na estação
-    robo.line = stationLine;
-    robo.column = stationColumn;
+     // Inicializa a posição do robô na estação
+    robot.line = stationLine;
+    robot.column = stationColumn;
+    verifyTrash();
 
-    while (1) {
+    while (trashCounter > 0) {
+        verifyTrash();
         int directionTrash = checkDirectionTrash();
-        int directionBase = checkDirectionBase();
-        
         // Se o robô não está na estação, limpa a posição atual
-        if (!(robo.line == stationLine && robo.column == stationColumn)) {
-            matrix[robo.line][robo.column] = "   ";
+        if (!(robot.line == stationLine && robot.column == stationColumn)) {
+            matrix[robot.line][robot.column] = 0;
         }
         
         // Move o robô baseado na direção do lixo
         switch (directionTrash) {
-            case 1: // Direita
-                robo.column += 1;
-                matrix[stationLine][stationColumn] = " E ";
+            case 1: // Cima
+                robot.line -= 1;
+                trashCounter -= 1;
                 break;
-            case 2: // Cima        
-                robo.line += 1;
-                matrix[stationLine][stationColumn] = " E ";
+            case 2: // Esquerda        
+                robot.column -= 1;
+                trashCounter -= 1;
                 break;
-            case 3: // Esquerda
-                robo.column -= 1;
-                matrix[stationLine][stationColumn] = " E ";
+            case 3: // Direita
+                robot.column += 1;
+                trashCounter -= 1;
                 break;
             case 4: // Baixo
-                robo.line -= 1;
-                matrix[stationLine][stationColumn] = " E ";
+                robot.line += 1;
+                trashCounter -= 1;
                 break;
             default:
-                returnBase();
+                // Se nenhuma direção for válida, re-verifica o lixo
+                verifyTrash();
+                break;
         }
-        
-        // Atualiza a posição do robô na matriz
-        matrix[robo.line][robo.column] = "-_-";
-        system("cls");
+        matrix[robot.line][robot.column] = -1;
         printMatrix();
         sleep(1); // Adiciona uma pausa para visualização do movimento
-        
-        if (!directionTrash && !directionBase) {
-        	return 0;
-		}
     }
+    return 0;
 }
 
 // Função para adicionar lixo na matriz
@@ -217,9 +188,6 @@ int addTrash() {
     
     // Continua a solicitar entradas do usuário até que a linha do lixo seja -1
     while (trashLine != -1) {
-        // Limpa a tela
-        system("cls");
-        // Imprime a matriz atualizada
         printMatrix();
         
         // Solicita ao usuário a linha do lixo
@@ -227,7 +195,7 @@ int addTrash() {
         scanf("%d", &trashLine);
         
         // Verifica se a linha do lixo está fora do intervalo permitido e solicita novamente se necessário
-        while (trashLine < -1 || trashLine > 7) {
+        while (trashLine < -1 || trashLine > SIZE - 1) {
             printf("Digite um numero entre 0 e 7 para a linha do lixo ou -1 para realizar limpeza: ");
             scanf("%d", &trashLine);
         }
@@ -235,7 +203,7 @@ int addTrash() {
         // Se a linha do lixo for -1, chama a função de limpeza
         if (trashLine == -1) {
             moveRobot();
-         	return 0;
+            return;
         }
         
         // Solicita ao usuário a coluna do lixo
@@ -243,7 +211,7 @@ int addTrash() {
         scanf("%d", &trashColumn);
         
         // Verifica se a coluna do lixo está fora do intervalo permitido e solicita novamente se necessário
-        while (trashColumn < 0 || trashColumn > 7) {
+        while (trashColumn < 0 || trashColumn > SIZE - 1) {
             printf("Digite um numero entre 0 e 7 para a coluna do lixo: ");
             scanf("%d", &trashColumn);
         }
@@ -254,20 +222,16 @@ int addTrash() {
             printf("Impossivel adicionar lixo na estacao!!\n");
         } else {
             // Define a posição do lixo na matriz com "@@@"
-            matrix[trashLine][trashColumn] = "@@@";
+            matrix[trashLine][trashColumn] = 2;
         }
     }
 }
 
-// Função principal do programa
+// Função principal
 int main() {
-    // Inicializa a matriz
     createMatrix();
-    // Define a posição inicial do robô
     addStation();
-    // Adiciona lixo na matriz
     addTrash();
-    // Retorna 0 para indicar que o programa terminou corretamente
-    system("cls");
     printMatrix();
+    return 0;
 }
